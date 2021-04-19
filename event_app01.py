867b1d0901623ac54299536aa6aa28dc815459a2
Taylor <taylorfaye96@gmail.com>
2021-04-19 14:34:06 -0400

    updated the links for registration and log in pages

--- event_app01.py
+++ event_app01.py
@@ -14,8 +14,14 @@ def register():
     return render_template('userReg.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('userLog.html')
+
+
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # routes for the pages
 # http://127.0.0.1:5000/home
 # http://127.0.0.1:5000/register
+# http://127.0.0.1:5000/login
