662aec8107df582876b8dfd48be328a8304332c3
Taylor <taylorfaye96@gmail.com>
2021-04-19 14:13:21 -0400

    added userReg.html file and basic functionality in event_app01.py

--- event_app01.py
+++ event_app01.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return "Functionality Check"
+    return render_template('home.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('userReg.html')
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
+
+# routes for the pages
+# http://127.0.0.1:5000/home
+# http://127.0.0.1:5000/register
