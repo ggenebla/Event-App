6510be8407816e1fe8fb4e8567461ab0debecd44
Taylor <taylorfaye96@gmail.com>
2021-04-21 23:44:50 -0400

    fixed a few broken things

--- models.py
+++ models.py
@@ -24,8 +24,6 @@ class User(db.Model):
     email = db.Column("email", db.String(100))
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
-    notes = db.relationship("Note", backref="user.id", lazy=True)
-    comments = db.relationship("Comment", backref="user", lazy=True)
 
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
